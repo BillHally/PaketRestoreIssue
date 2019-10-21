ad8be17ae72f6761dd7913227e122077b0e44bab
Bill Hally <bill.hally@gmail.com>
2019-10-21 14:42:14 +0100

    Repro for a bug in Fake 5.18.0 which causes Paket.restore to fail

---
+++ build.fsx
@@ -0,0 +1,13 @@
+#load ".fake/build.fsx/intellisense.fsx"
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
+
+Target.initEnvironment ()
+
+Target.create "PaketRestore" (fun _ -> Paket.restore id)
+
+Target.runOrDefault "PaketRestore"
